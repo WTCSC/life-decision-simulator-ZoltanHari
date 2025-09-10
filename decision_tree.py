44f3d3b325ad0e702aa07e1d17fa2da8f0c8b089
ZoltanHari <harizozi1009@gmail.com>
2025-09-10 13:23:25 -0600

    Start Again

--- decision_tree.py
+++ decision_tree.py
@@ -1,2 +1,1 @@
-print('"Intruder Alert! A RED Spy Is In The Base" \nA Red Spy Has Breached Your Defenses and Is Attempting to Steal Your Intelligence Briefcase.') 
-choice_1 = input("What Will You Do?")
+
