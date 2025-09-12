18d87faa10975cfeff37a696c2117c08b86042f4
Zoltan Hari <harizozi1009@gmail.com>
2025-09-12 08:12:54 -0600

    Good Code

--- decision_tree.py
+++ decision_tree.py
@@ -1,3 +1,16 @@
+import re 
+
 def read_file(msg_num):
     with open("Messages", "r") as file:
-        messages = file.read()
+        message = file.read().split(f"%msg{msg_num}%")[1]
+        try:
+            responses = message.split("1.")[1].split("2.")
+            option = input(re.sub(r"%..%","",re.sub(r"%.%","",message)))
+            if option == "1":
+                read_file(responses[0].split("%")[1])
+            else:
+                read_file(responses[1].split("%")[1])
+        except:
+            print(re.sub(r"%..%","",re.sub(r"%.%","",message)))
+
+read_file("0")
